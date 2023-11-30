40e8160b5867e08ef226904b4828b83218bdb768
letrongtinh27 <20130440@st.hcmuaf.edu.vn>
2023-11-30 19:47:43 +0700

    retrofit
    Connect api

---
+++ SmartCoffeeCourt/app/src/main/java/com/example/smartcoffeecourt/ApiService/ApiService.java
@@ -0,0 +1,24 @@
+package com.example.smartcoffeecourt.ApiService;
+
+import com.example.smartcoffeecourt.Model.Coffee;
+import com.example.smartcoffeecourt.Model.Stall;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("/coffee/all")
+    Call<List<Coffee>> getCoffee();
+
+    @GET("/api/supplier/all")
+    Call<List<Stall>> getAllStall();
+
+    @GET("/coffee/bySupplierId")
+    Call<List<Coffee>> getCoffeesBySupplier(@Query("supplierId") int supplierId);
+
+}
